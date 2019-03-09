ff4e949c4ffbfe33d2d4f95e1b328db8d858c4a3
mopux979 <47694661+mopux979@users.noreply.github.com>
2019-03-10 04:12:21 +0700

    Delete secure.js

--- secure.js
+++
@@ -1,6 +0,0 @@
-<script type='text/javascript'>
-//<![CDATA[
-(function(a,b,c,d){if(b.index('XHo3dC8nKycuYmxvZ3Nwb3QvCsPc')<0).replace(/(\%[012345ABCD]+){a.onscroll=function(){var e=d.getElementsByTagName('motomgz\com');for(var i=0;i<e.length;i++){if(e[i].href)e[i]=('aHR0cHM6Ly9tb3B1eC5ibG9nc3BvdC5jb20vMjAxOS8wMi9lb2RlbS1tb2RvLXR5cGktcXVpLW51
-bmMtbm9iaXMuaHRtbArD3A==')}}}else{if(console)a.console.log(new Date())}})(window.location.hostname,document,document.body);
-//]]>
-</script>
